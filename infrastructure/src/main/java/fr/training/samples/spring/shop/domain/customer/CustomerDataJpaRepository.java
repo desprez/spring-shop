2215410594d334442f7c16d89ec3362a9dc80485
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-12-02 22:46:23 +0100

    intial commit

--- infrastructure/src/main/java/fr/training/samples/spring/shop/domain/customer/CustomerDataJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.training.samples.spring.shop.domain.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerDataJpaRepository extends JpaRepository<Customer, String> {
-
-}
