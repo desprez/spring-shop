5f06c9f7e71568e049317552304f2b6eae9b1ee3
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-12-04 15:44:15 +0100

    remove useless accessors

--- domain/src/main/java/fr/training/samples/spring/shop/domain/customer/Customer.java
+++ domain/src/main/java/fr/training/samples/spring/shop/domain/customer/Customer.java
@@ -12,38 +12,6 @@ public class Customer extends AbstractBaseEntity {
 	private String password;
 
 	/**
-	 * @return the id
-	 */
-	@Override
-	public String getId() {
-		return id;
-	}
-
-	/**
-	 * @param id the id to set
-	 */
-	@Override
-	public void setId(final String id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the version
-	 */
-	@Override
-	public Integer getVersion() {
-		return version;
-	}
-
-	/**
-	 * @param version the version to set
-	 */
-	@Override
-	public void setVersion(final Integer version) {
-		this.version = version;
-	}
-
-	/**
 	 * @return the name
 	 */
 	public String getName() {
