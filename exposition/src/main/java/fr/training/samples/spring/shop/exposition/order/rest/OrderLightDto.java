1383db8d21f1fdeb047b9840a74c8d9be1c84c25
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-12-04 17:29:12 +0100

    clean up

--- exposition/src/main/java/fr/training/samples/spring/shop/exposition/order/rest/OrderLightDto.java
+++ exposition/src/main/java/fr/training/samples/spring/shop/exposition/order/rest/OrderLightDto.java
@@ -17,30 +17,18 @@ public class OrderLightDto implements Serializable {
 	public OrderLightDto() {
 	}
 
-	/**
-	 * @return the customerId
-	 */
 	public String getCustomerId() {
 		return customerId;
 	}
 
-	/**
-	 * @param customerId the customerId to set
-	 */
 	public void setCustomerId(final String customerId) {
 		this.customerId = customerId;
 	}
 
-	/**
-	 * @return the itemIds
-	 */
 	public List<String> getItemIds() {
 		return itemIds;
 	}
 
-	/**
-	 * @param itemIds the itemIds to set
-	 */
 	public void setItemIds(final List<String> itemIds) {
 		this.itemIds = itemIds;
 	}
