1687a710b07c98c5f767eb4239c98631bd4152c0
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-12-04 17:38:00 +0100

    clean up

--- exposition/src/main/java/fr/training/samples/spring/shop/exposition/item/rest/ItemResource.java
+++ exposition/src/main/java/fr/training/samples/spring/shop/exposition/item/rest/ItemResource.java
@@ -17,10 +17,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import fr.training.samples.spring.shop.application.item.ItemService;
 import fr.training.samples.spring.shop.domain.item.Item;
-import fr.training.samples.spring.shop.exposition.common.ErrorModel;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api")
@@ -33,31 +29,21 @@ public class ItemResource {
 	private final ItemMapper itemMapper;
 
 	public ItemResource(final ItemService itemService, final ItemMapper itemMapper) {
-		super();
 		this.itemService = itemService;
 		this.itemMapper = itemMapper;
 	}
 
-	@ApiOperation(value = "This operation allow to retrieve all items", nickname = "showItems", notes = "All exiting item should be returned")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "OK", response = ItemDto.class, responseContainer = "List"),
-			@ApiResponse(code = 403, message = "Forbidden", response = ErrorModel.class),
-			@ApiResponse(code = 404, message = "Not Found ", response = ErrorModel.class),
-			@ApiResponse(code = 500, message = "Internal Server Error", response = ErrorModel.class) })
 	@GetMapping(value = "/items", produces = { "application/json" })
 	public List<ItemDto> getAllItemsUsingGet() {
+
 		final List<Item> items = itemService.getAllItems();
 		logger.info("Number of items returned: {}", items.size());
 		return itemMapper.mapToDtoList(items);
+
 	}
 
-	@ApiOperation(value = "This operation allow to add a new item", nickname = "addItem", notes = "Please give item infos")
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Item was added"),
-			@ApiResponse(code = 403, message = "Forbidden", response = ErrorModel.class),
-			@ApiResponse(code = 404, message = "Not Found ", response = ErrorModel.class),
-			@ApiResponse(code = 500, message = "Internal Server Error", response = ErrorModel.class) })
 	@PostMapping(value = "/items", produces = { "application/json" })
-	public ResponseEntity<URI> addItem(@Valid @RequestBody final ItemLightDto itemDto) {
+	public ResponseEntity<URI> addItemUsingPost(@Valid @RequestBody final ItemLightDto itemDto) {
 
 		final Item item = itemMapper.mapToEntity(itemDto);
 		itemService.addItem(item);
