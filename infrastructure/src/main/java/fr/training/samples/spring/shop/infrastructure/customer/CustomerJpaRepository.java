fb72aa491713ad8a31d01b59b103cca497b7a756
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-12-05 18:51:41 +0100

    add order feature

--- infrastructure/src/main/java/fr/training/samples/spring/shop/infrastructure/customer/CustomerJpaRepository.java
+++ infrastructure/src/main/java/fr/training/samples/spring/shop/infrastructure/customer/CustomerJpaRepository.java
@@ -1,4 +1,4 @@
-package fr.training.samples.spring.shop.insfrastructure.customer;
+package fr.training.samples.spring.shop.infrastructure.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
